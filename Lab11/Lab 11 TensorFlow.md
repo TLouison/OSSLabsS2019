2dbf04d7e9de00c39fdf191dbbb3aad831bcba12
TLouison <toddmlouison@gmail.com>
2019-04-12 21:56:17 -0400

    Completed lab 11

---
+++ Lab11/Lab 11 TensorFlow.md
@@ -0,0 +1,35 @@
+# Lab 11: TensorFlow
+
+## Todd Louison
+
+### Checkpoint 1
+
+Here is the figure showing the correct install of TensorFlow
+
+![check1](/Users/todd/Documents/School/Junior/S2/OSS/Labs/Lab11/check1.png)
+
+### Checkpoint 2
+
+Here is the plot of the TensorFlow Classification
+
+![check2](/Users/todd/Documents/School/Junior/S2/OSS/Labs/Lab11/check.png)
+
+### Checkpoint 3
+
+Here are the three attempts at classifiying articles of clothing:
+
+![Purse](/Users/todd/Documents/School/Junior/S2/OSS/Labs/Lab11/check3Plots/purseFINAL.png)
+
+![smallPurse](/Users/todd/Documents/School/Junior/S2/OSS/Labs/Lab11/data/purse_small.jpg)
+
+---
+
+![jacket](/Users/todd/Documents/School/Junior/S2/OSS/Labs/Lab11/check3Plots/jacketFINAL.png)
+
+![smallJacket](/Users/todd/Documents/School/Junior/S2/OSS/Labs/Lab11/data/jacket_small.jpg)
+
+---
+
+![pants](/Users/todd/Documents/School/Junior/S2/OSS/Labs/Lab11/check3Plots/pantsFINAL.png)
+
+![smallPants](/Users/todd/Documents/School/Junior/S2/OSS/Labs/Lab11/data/pants_small.jpg)
---
+++ Lab11/check3.py
@@ -0,0 +1,126 @@
+
+#@title Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#@title MIT License
+#
+# Copyright (c) 2017 François Chollet
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+
+def predictClothing(image, true):
+    # Add the image to a batch where it's the only member.
+    image = (np.expand_dims(image, 0))
+    prediction = model.predict(image)
+
+    #plot_image(0, prediction, true, image)
+    plot_value_array(0, prediction, true)
+
+    plt.xticks(range(10), class_names, rotation=45)
+    plt.show()
+
+    result = np.argmax(prediction[0])
+    print(result)
+
+def plot_image(i, predictions_array, true_label, img):
+  predictions_array = predictions_array[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+
+  plt.imshow(img, cmap=plt.cm.binary)
+
+  predicted_label = np.argmax(predictions_array)
+  if predicted_label == true_label:
+    color = 'blue'
+  else:
+    color = 'red'
+
+  plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                100*np.max(predictions_array),
+                                class_names[true_label]),
+                                color=color)
+
+def plot_value_array(i, predictions_array, true_label):
+  predictions_array = predictions_array[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+  thisplot = plt.bar(range(10), predictions_array, color="#777777")
+  plt.ylim([0, 1])
+  predicted_label = np.argmax(predictions_array)
+
+  thisplot[predicted_label].set_color('red')
+  thisplot[true_label].set_color('blue')
+
+
+fashion_mnist = keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=5)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+print('Test accuracy:', test_acc)
+
+jacket = (255 - np.reshape(cv2.imread("data/jacket_small.jpg",0),(28,28)))/255.0
+pants = (255 - np.reshape(cv2.imread("data/pants_small.jpg",0),(28,28)))/255.0
+purse = (255 - np.reshape(cv2.imread("data/purse_small.jpg",0),(28,28)))/255.0
+
+print(class_names)
+
+predictClothing(jacket, 4)
+predictClothing(pants, 1)
+predictClothing(purse, 8)
